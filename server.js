c895bc4d6d38a61f201047ba089027612c047c7e
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-17 14:00:16 -0700

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config({ path: `${__dirname}/config/config.env` });
+const connectDB = require("./config/db");
 const app = require("./app.js");
+
+connectDB();
 
 const PORT = process.env.PORT || 500;
 const server = app.listen(
